33566abb733c57a568de51988a37eadf22716cd6
Maxim Lebiga <lebiga.maks@gmail.com>
2021-06-09 18:26:53 +0300

    added menu items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+
+
+import menuItem from "./templates/menu-item.hbs"
+import ingredients from "./js/menu.json"
+
+const menuList = document.querySelector('.js-menu')
+
+const createMenuMarkUp = function (object) {
+    return object.map(menuItem).join('')
+}
+
+const menuItems = createMenuMarkUp(ingredients)
+
+menuList.insertAdjacentHTML('beforeend', menuItems)
+console.log(menuItems)
